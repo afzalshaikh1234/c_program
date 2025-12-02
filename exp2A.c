089a8efc9eee448cdfbf6778052198ccacb4485f
afzalshaikh1234 <shaikhshaikh7370@gmail.com>
2025-12-02 09:51:28 +0530

    Add files via upload

---
+++ exp2A.c
@@ -0,0 +1,22 @@
+/*
+Name:Shaikh Mohammed Afzal Akbar
+UIN:251P120
+Class:Computer Engineering First Year
+Division:D
+exp:2A
+*/
+#include <stdio.h>
+
+ int main() {
+
+int num;
+
+printf("Enter the num: ");
+
+scanf("%d", &num);
+
+(num%2==0) ?printf("Even"); printf("0dd"):
+
+return 0;
+
+}
