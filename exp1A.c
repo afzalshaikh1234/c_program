4aa204a689bb5b8f8696d47cffdaba848a120608
afzalshaikh1234 <shaikhshaikh7370@gmail.com>
2025-12-02 09:34:08 +0530

    Add files via upload

---
+++ exp1A.c
@@ -0,0 +1,21 @@
+/*
+Name:Shaikh Mohammed Afzal Akbar
+UIN:251P120
+Class:Computer Engineering First Year
+Division:D
+*/
+#include <stdio.h>
+int main() {
+
+ printf("Name of Student: Shaikh Mohammed Afzal Akbar\n"):
+
+ printf("UIN: 251P120 \n"):
+
+ printf("Class: Computer Engineering First Year \n"):
+
+ printf("Division: D \n"):
+
+ printf("Age: 19 \n"):
+return 0;
+
+}
