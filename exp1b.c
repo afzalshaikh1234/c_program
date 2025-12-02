977ac1edef56342c46633a9a717b4571d5200156
afzalshaikh1234 <shaikhshaikh7370@gmail.com>
2025-12-02 09:42:53 +0530

    Add files via upload

---
+++ exp1b.c
@@ -0,0 +1,26 @@
+/*
+Name:Shaikh Mohammed Afzal Akbar
+UIN:251P120
+Class:Computer Engineering First Year
+Division:D
+exp:1B
+*/
+#include <stdio.h>
+
+int main() {
+int phy.chem.maths:
+float avg:
+   printf("Enter physics marks:"):
+  scanf("%d", &phy);
+   printf("Enter Chemistry marks: ");
+  scanf("%d", &chem):
+   printf("Enter Maths marks:");
+ scanf("%d", &maths);
+   avg (phy chem + maths) / 3:
+   printf("Average = %.2f .2f\n",avg):
+
+(avg >= 50)? printf("Eligibl igible"): printf("Not Eligible");
+
+ }
+
+return 0;
